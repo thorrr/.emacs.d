b5f595ceafcb8e7726e7f5f29b0f9567300e0320
Jason Bell <jbell9999@gmail.com>
2013-01-16 17:29:33 -0500

    move line number face into toggle code.  can't figure out why it's getting reset

--- newstuff.el
+++ newstuff.el
@@ -149,13 +149,14 @@
 
 
 ;; let's try line numbers
-(face-spec-reset-face 'linum)
-(set-face-attribute 'linum nil :inherit nil :background "#4f4f4f"
-                    :foreground "slate gray")
 
 (defun toggle-line-numbers ()
   (interactive)
   (require 'linum)
+  (face-spec-reset-face 'linum)
+  (set-face-attribute 'linum nil :inherit nil :background "#4f4f4f"
+                      :foreground "slate gray")
+  
   (let ((linum-format (concat "%" (format "%s" (ceiling (log10 (line-number-at-pos (point-max))))) "d")))
     (set-fringe-style '(2 . 10)) ;;TODO - save fringe style
     (if linum-mode (linum-mode 0) (linum-on))))
