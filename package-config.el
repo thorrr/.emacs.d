948d3ffce6115ec0db414fed30b1feecca7a33b2
Jason Bell <jasonmurraybell@gmail.com>
2013-08-20 19:40:50 -0400

    fix iflipb timer code

--- package-config.el
+++ package-config.el
@@ -124,36 +124,34 @@
 
 (setq iflipb-wrap-around t)
 ;; auto off function iflipb'ing
-(setq my-iflipb-auto-off-timeout-sec 1)
-(setq my-iflipb-auto-off-timer-canceler-internal nil)
-(setq my-iflipb-ing-internal nil)
-(defun my-iflipb-auto-off ()
-  (message nil)
-  (setq my-iflipb-auto-off-timer-canceler-internal nil
-        my-iflipb-ing-internal nil)
+(setq my-iflipb-timeout 0.8)
+(setq my-iflipb-timer-object nil)
+(defun my-iflipb-timer ()
+  (setq my-iflipb-timer-running nil)
+  (cancel-timer my-iflipb-timer-object)
+  (setq my-iflipb-timer-object nil)
   )
 (defun my-iflipb-next-buffer (arg)
   (interactive "P")
   (iflipb-next-buffer arg)
-  (if my-iflipb-auto-off-timer-canceler-internal
-      (cancel-timer my-iflipb-auto-off-timer-canceler-internal))
-  (run-with-idle-timer my-iflipb-auto-off-timeout-sec 0 'my-iflipb-auto-off)
-  (setq my-iflipb-ing-internal t)
-  )
+  ;;reset timer if it's running
+  (if my-iflipb-timer-object
+      (cancel-timer my-iflipb-timer-object))
+  (setq my-iflipb-timer-object (run-with-idle-timer my-iflipb-timeout nil 'my-iflipb-timer)))
+
 (defun my-iflipb-previous-buffer ()
   (interactive)
   (iflipb-previous-buffer)
-  (if my-iflipb-auto-off-timer-canceler-internal
-      (cancel-timer my-iflipb-auto-off-timer-canceler-internal))
-  (run-with-idle-timer my-iflipb-auto-off-timeout-sec 0 'my-iflipb-auto-off)
-  (setq my-iflipb-ing-internal t)
-  )
+  ;;reset timer if it's running
+  (if my-iflipb-timer-object
+      (cancel-timer my-iflipb-timer-object))
+  (setq my-iflipb-timer-object (run-with-idle-timer my-iflipb-timeout nil 'my-iflipb-timer)))
+
 (defun iflipb-first-iflipb-buffer-switch-command ()
-  "Determines whether this is the first invocation of
-  iflipb-next-buffer or iflipb-previous-buffer this round."
+  "Override existing function - add check for my-iflipb-timer-running"
   (not (and (or (eq last-command 'my-iflipb-next-buffer)
                 (eq last-command 'my-iflipb-previous-buffer))
-            my-iflipb-ing-internal)))
+            my-iflipb-timer-object)))
 
 ;; revert buffers automatically when underlying files are changed externally
 (global-auto-revert-mode t)
