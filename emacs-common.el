70aebc70860489e55e8ad123ad1f7ebe34a8a341
Jason Bell <jbell9999@gmail.com>
2013-12-25 20:43:00 -0500

    move package code to packages.el

--- emacs-common.el
+++ emacs-common.el
@@ -75,102 +75,11 @@
     mv color-theme-6.6.0 color-theme-tmp && cd color-theme-tmp && mv color-theme-6.6.0 .. &&
     cd .. && rmdir color-theme-tmp"
 )))
-
-;; make the externals directory
-(setq shared-externals (expand-file-name shared-externals))
-(unless (file-exists-p shared-externals)
-  (make-directory shared-externals 't))
-
-;; If we're on version 23, download the latest package.el and load it
-(if (< emacs-major-version 24)
-    (let* ((default-directory (expand-file-name shared-externals))
-           (package-file (concat default-directory "pkg-el23")))
-      (unless (file-exists-p package-file)
-        (shell-command-to-string "wget http://bit.ly/pkg-el23"))
-      (load package-file)))
-
-(require 'package)
-(setq package-archives (append package-archives  '(
-    ("melpa" . "http://melpa.milkbox.net/packages/"))
-))
-
-;; change default elpa directory and load packages
-(setq package-user-dir shared-externals)
-(package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
-;; install new packages
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
-
-(defun git-clone (project-name project-url)
-  (unless (file-exists-p (concat (expand-file-name default-directory) project-name))
-    (let ((cmd (concat "git clone -q " project-url " " project-name)))
-      (message (concat "Running git for project " project-name " in directory " default-directory))
-      (shell-command-to-string cmd))))
-
-(defun git-update (project-name project-url)
-    (let ((cmd (concat "git pull -q"))
-          (dir (concat default-directory project-name)))
-      (if (file-exists-p dir)
-          (let ((default-directory (expand-file-name dir)))
-            (shell-command-to-string cmd)))))
-
-(defun hg-clone (project-name project-url)
-  (unless (file-exists-p (concat (expand-file-name default-directory) project-name))
-    (let ((cmd (concat "hg clone -q " project-url " " project-name)))
-      (message (concat "Running hg for project " project-name " in directory " default-directory))
-      (shell-command-to-string cmd))))
-
-(defun hg-update (project-name project-url)
-    (let ((cmd (concat "hg pull -q"))
-          (dir (concat default-directory project-name)))
-      (if (file-exists-p dir)
-          (let ((default-directory (expand-file-name dir)))
-            (shell-command-to-string cmd)))))
-
-(defun wget-clone (project-name project-url)
-  (let ((dir (concat (expand-file-name default-directory) project-name)))
-  (unless (file-exists-p dir)
-    (let ((cmd-1 (concat "mkdir " dir))
-          (cmd-2 (concat "cd " dir "&& wget " project-url)))
-      (message (concat "Running wget for project " project-name " in directory " dir))
-      (shell-command-to-string cmd-1)
-      (shell-command-to-string cmd-2)))))
-
-
-(defun run-local-package-commands (list-of-commands)
-  (let ((default-directory (expand-file-name shared-externals)))
-    (mapcar 'shell-command-to-string list-of-commands)))
-
-;; Install packages from git and hg
-(let ((default-directory (expand-file-name shared-externals)))
-  (unless (file-exists-p shared-externals)
-    (make-directory shared-externals 't))
-  (mapcar (lambda (e) (git-clone (car e) (cadr e))) git-projects)
-  (mapcar (lambda (e) (hg-clone (car e) (cadr e))) hg-projects)
-  (mapcar (lambda (e) (wget-clone (car e) (cadr e))) wget-projects)
-)
-
-  
-;; Run commands in the externals directory
-(run-local-package-commands make-projects)
-
-;; Add externals to load path
-(mapcar (lambda (e)
-          (add-to-list 'load-path (expand-file-name (concat shared-externals (car e)))))
-        (append git-projects hg-projects wget-projects))
-
-;;(add-to-list 'load-path (concat emacs-config-root "misc-packages/"))
-
+(setq emacs-config-root (file-name-directory load-file-name))
+(load (concat emacs-config-root "packages.el"))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;  Global Customizations
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Utility functions that all subsequent files can rely on
-(setq emacs-config-root (file-name-directory load-file-name))
-(load (concat emacs-config-root "elisp-utils.el"))
-
 ;; clean up dirnames
 (setq emacs-savefile-dir (expand-file-name emacs-savefile-dir))
 
