3ec55b4e6067d476d8a1114d3bfdc29ec99fa49e
Jason Bell <jbell9999@gmail.com>
2013-12-25 19:49:55 -0500

    fix whitespace in elisp-utils

--- elisp-utils.el
+++ elisp-utils.el
@@ -1,3 +1,5 @@
+
+
 ;;from http://emacswiki.org/emacs/ThreadMacroFromClojure
 (defmacro ->> (&rest body)
   (let ((result (pop body)))
@@ -22,8 +24,8 @@
     (recur)))
 
 (defun chomp (str)
-      "Chomp leading and tailing whitespace from STR."
-      (while (string-match "\\`\n+\\|^\\s-+\\|\\s-+$\\|\n+\\'"
-                           str)
-        (setq str (replace-match "" t t str)))
-      str)
+  "Chomp leading and tailing whitespace from STR."
+  (while (string-match "\\`\n+\\|^\\s-+\\|\\s-+$\\|\n+\\'"
+                       str)
+    (setq str (replace-match "" t t str)))
+  str)
