41620c33533a99aaab09d0dd8df82ec946f20082
thorrr <jbellthor@gmail.com>
2015-08-26 10:15:53 -0400

    get rid of keep undo revert function, it doesn't work

--- newstuff.el
+++ newstuff.el
@@ -207,16 +207,3 @@
 
 ;; new ob-ipython stuff
 (require 'ob-ipython)
-
-
-;; try this out, see if it works
-(if (or (< emacs-major-version 24) (< emacs-minor-version 3))
-    (defun revert-buffer-keep-undo (&rest -)
-      "Revert buffer but keep undo history."
-      (interactive)
-      (let ((inhibit-read-only t))
-        (erase-buffer)
-        (insert-file-contents (buffer-file-name))
-        (set-visited-file-modtime (visited-file-modtime))
-        (set-buffer-modified-p nil)))
-  (setq revert-buffer-function 'revert-buffer-keep-undo))
