b67c91e6a8196a571e39774cc1518f4932092cf4
thorrr <jbellthor@gmail.com>
2015-09-23 10:44:32 -0400

    fix magit commit buffer with broken cygwin path

--- newstuff.el
+++ newstuff.el
@@ -158,6 +158,16 @@ by changing them to C:/*"
   (when (string-match "^/cygdrive/\\([a-z]\\)/\\(.*\\)" filename)
     (setq filename (concat (match-string 1 filename) ":/"
                            (match-string 2 filename)))))
+
+(defun un-cygwin-buffer-file-name ()
+  (when (string-match "^\\([a-z]\\):/cygdrive/\\([a-z]\\)/\\(.*\\)" buffer-file-name)
+    ;; assertion:  filename should look like "c:/cygwin/c/Users..." i.e. the drive is repeated
+    (if (equal (match-string 1 buffer-file-name) (match-string 2 buffer-file-name)) (progn
+      (set-visited-file-name
+            (concat (match-string 1 buffer-file-name) ":/"
+                    (match-string 3 buffer-file-name)) 't)))))
+(add-hook 'git-commit-mode-hook 'un-cygwin-buffer-file-name)
+
 ;; (require 'git-gutter-fringe)
 ;; (global-git-gutter-mode +1)
 ;; (git-gutter:linum-setup)
