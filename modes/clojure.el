927987ae9ba5d5516b403a050ae15e51f2a8ee31
Jason Bell <jbellthor@gmail.com>
2015-11-11 15:46:25 -0500

    add cider to clojure mode

--- modes/clojure.el
+++ modes/clojure.el
@@ -1,3 +1,7 @@
 (autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-(add-hook 'clojure-mode-hook #'paredit-mode )
+
+(add-hook 'clojure-mode-hook #'paredit-mode)
+
+;; this version roughly tracks "master"
+(add-to-list 'package-pinned-packages '(cider . "melpa") 't)
