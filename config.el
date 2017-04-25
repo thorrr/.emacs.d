80b22b73cc7421c44c36583feba8cee432a5361b
Jason Bell <jbellthor@gmail.com>
2017-04-25 15:37:05 -0400

    undo-tree load not working with emacs 25

--- config.el
+++ config.el
@@ -50,9 +50,9 @@
 (add-hook 'temp-buffer-setup-hook (lambda ()
   (prefer-coding-system 'utf-8)))
 ;;this won't work until 24.3
-(if (or (>= emacs-major-version 25) (and (>= emacs-major-version 24) (>= emacs-minor-version 3)))
-    (setq undo-tree-auto-save-history 't) 
-    (setq undo-tree-auto-save-history nil))
+(if (and (= emacs-major-version 24) (>= emacs-minor-version 3)) ;; undo-tree history is crashing emacs25
+    (setq undo-tree-auto-save-history 't)
+  (setq undo-tree-auto-save-history nil))
 (add-hook 'write-file-hooks 'undo-tree-save-history-hook)
 (add-hook 'find-file-hook 'undo-tree-load-history-hook)
 
