6316f620ae6f11d94e8df1cd58f97530c6ce277b
Jason Bell <jbellthor@gmail.com>
2017-08-22 11:38:41 -0400

    make packages use https

--- packages.el
+++ packages.el
@@ -13,10 +13,10 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 
 ;; change default elpa directory and load packages
