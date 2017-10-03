4f7b4b67685fe70be2a88d7dd90034d6fefd8d8d
Jason Bell <jbellthor@gmail.com>
2017-10-03 15:25:01 -0400

    tramp customizations

--- newstuff.el
+++ newstuff.el
@@ -240,3 +240,20 @@ to specify a custom port"
 
 (require 'dired-k)
 (add-hook 'dired-initial-position-hook 'dired-k)
+
+
+;; tramp customizations 
+(setq tramp-persistency-file-name "~/.local-emacs/tramp")
+(if (eq system-type 'windows-nt)
+    (eval-after-load "tramp"
+      '(progn
+         (add-to-list 'tramp-methods
+           (mapcar
+            (lambda (x)
+              (cond
+               ((equal x "sshx") "cygssh")
+               ((eq (car x) 'tramp-login-program) (list 'tramp-login-program "fakecygpty ssh"))
+               (t x)))
+            (assoc "sshx" tramp-methods)))
+         (setq tramp-default-method "cygssh"))))
+  
