734d0e6ce02391003eb6d7e2873809d348af9f8f
Jason Bell <jbellthor@gmail.com>
2018-02-26 20:58:50 -0500

    fix M-q shortcut to indent

--- modes/javascript.el
+++ modes/javascript.el
@@ -14,6 +14,7 @@
 ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
 ;; unbind it.
 ;; (define-key js-mode-map (kbd "M-.") nil)
+(define-key js-mode-map (kbd "M-q") 'indent-region)
 
 (add-hook 'js2-mode-hook (lambda ()
   (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
