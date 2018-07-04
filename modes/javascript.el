a2696ed2c0b920a1050660aa19cbe438cdb2e576
Jason Bell <jbellthor@gmail.com>
2018-07-03 20:48:10 -0400

    make C-<right> bind to sp-slurp-hybrid-sexp just for js

--- modes/javascript.el
+++ modes/javascript.el
@@ -11,14 +11,20 @@
 (js2r-add-keybindings-with-prefix "C-c C-r")
 (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
 
-;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
-;; unbind it.
-;; (define-key js-mode-map (kbd "M-.") nil)
-(define-key js-mode-map (kbd "M-q") 'indent-region)
-(define-key js-mode-map (kbd "C-<right>") 'sp-slurp-hybrid-sexp)
-
 (add-hook 'js2-mode-hook (lambda ()
-  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+  ;; this makes js2-mode work with the indent-region from rjsx-mode
+  (define-key js-mode-map (kbd "M-q") 'indent-region)
+
+  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)
+
+  ;; buffer-local override for C-right in smartparens-mode-map
+  (let ((oldmap (cdr (assoc 'smartparens-mode minor-mode-map-alist)))
+        (newmap (make-sparse-keymap)))
+    (set-keymap-parent newmap oldmap)
+    (define-key newmap (kbd "C-<right>") 'sp-slurp-hybrid-sexp)
+    (make-local-variable 'minor-mode-overriding-map-alist)
+    (push `(smartparens-mode . ,newmap) minor-mode-overriding-map-alist))
+))
 
 (require 'rjsx-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
