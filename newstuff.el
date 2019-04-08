0501dee37375b150ca525f7e5a241d9a82515ae0
Jason Bell <jasonmbell@google.com>
2019-04-08 12:10:44 -0400

    fci-mode fixes

--- newstuff.el
+++ newstuff.el
@@ -234,6 +234,13 @@ by changing them to C:/*"
             (turn-off-fci-mode)))))))
 (add-hook 'window-size-change-functions 'fci-width-workaround)
 
+;; fix for company-mode completion bugs
+(defun on-off-fci-before-company(command)
+  (when (string= "show" command)
+    (turn-off-fci-mode))
+  (when (string= "hide" command)
+    (turn-on-fci-mode)))
+(advice-add 'company-call-frontends :before #'on-off-fci-before-company)
 
 ;; wrapper for ssh on nt using fakecygpty
 (if (eq system-type 'windows-nt)
