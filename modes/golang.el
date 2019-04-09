cfc6d03a0a915fe4aee1e6cbafdb820d2adbbf5d
Jason Bell <jasonmbell@google.com>
2019-04-09 12:06:06 -0400

    cleanup golang and add key shortcuts

--- modes/golang.el
+++ modes/golang.el
@@ -1,9 +1,18 @@
+(require 'company-ycmd)
+(company-ycmd-setup)
+
 (use-package go-mode
   :config
   (add-hook 'before-save-hook #'gofmt-before-save)
   (add-hook 'go-mode-hook 'flycheck-mode)
   (add-hook 'go-mode-hook 'dumb-jump-mode)
-  (add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
+  (add-hook 'go-mode-hook
+            (lambda ()
+              (setq tab-width 4)
+              (keymaps-mode 0)
+              (golang-minor-mode)
+              (company-mode-on)
+              (auto-complete-mode -1)))
   (setq go-packages-function 'go-packages-go-list))
 
 
@@ -21,3 +30,21 @@
   (shell-command "go test . -coverprofile=cover.out")
   (go-coverage "cover.out")
   (rotate:even-horizontal))
+
+
+;; can't figure out how to call-interactively xref-find-definitions (timing issue?)  so
+;; define a new minor mode and put special keybindings there
+
+(defvar golang-minor-mode-map (make-sparse-keymap)
+  "Keymap while golang-temp-mode is active.")
+(set-keymap-parent golang-minor-mode-map keymaps-mode-map)
+
+(define-key golang-minor-mode-map (kbd "M-.") 'xref-find-definitions)
+(define-key golang-minor-mode-map (kbd "M-,") 'xref-pop-marker-stack)
+
+(define-minor-mode golang-minor-mode
+  "A temporary minor mode to be activated only in golang buffers."
+  :init-value nil ;;docs say this should be nil in almost all situations
+  :lighter " keymaps-mode" ;; make this mode look like keymaps-mode
+  :keymap golang-minor-mode-map)
+(provide 'golang-minor-mode)
