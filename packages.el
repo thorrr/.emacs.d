7f9fe570b776015eee5b5684c07888901e0cffd3
Jason Bell <jasonmbell@google.com>
2019-06-10 17:08:34 -0400

    fix error using fboundp to check for loccur-mode

--- packages.el
+++ packages.el
@@ -438,13 +438,13 @@
   :bind (:map loccur-mode-map
         ("C-g" . (lambda ()
                    (interactive)
-                   (if (fboundp loccur-mode)
+                   (if (fboundp 'loccur-mode)
                        (if (loccur-current)) (keyboard-quit))))
          :map isearch-mode-map
         ("C-o" .  loccur-current)
         ("C-g" . (lambda ()
                    (interactive)
-                   (if (fboundp loccur-mode)
+                   (if (fboundp 'loccur-mode)
                        (if loccur-mode (loccur-current)) (keyboard-quit))))))
 
 (use-package magit
